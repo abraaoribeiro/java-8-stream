80d829e5db56059f8919de3fb21c5d59a0c5b6e9
Abraão Souza Ribeiro <abraao.ribeiro@tre-pa.jus.br>
2018-12-17 08:18:57 -0300

    Expressões lambda

--- src/abraao/pa/expressoes/lambda/LambdaExpression.java
+++ src/abraao/pa/expressoes/lambda/LambdaExpression.java
@@ -1,9 +1,20 @@
 package abraao.pa.expressoes.lambda;
 
-interface Drawable{
+interface Drawable {
 	public void draw();
 }
 
 public class LambdaExpression {
+
+	public static void main(String[] args) {
+		int width = 10;
+
+		Drawable withLambda = () -> {
+			System.out.println("Drawing" + width);
+		};
+
+		withLambda.draw();
+
+	}
 
 }
